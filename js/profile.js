af834466105e5e7ec5e442cb880913f6fe531476
Roel Van Rossen <roelifant@gmail.com>
2017-04-26 13:51:12 +0200

    voornaam, naam en email aanpassen zonder submit knop

--- js/profile.js
+++ js/profile.js
@@ -3,4 +3,22 @@ $(document).ready(function(){
     $("#img").click(function(){
         $("#image").css('visibility', 'visible');
     });
+
+    $("#firstname, #lastname, #email").blur(function(){
+        if(e.keyCode==13){
+            
+        var lastname = $("#lastname").val()
+        var email = $("#email").val()
+        
+        alert(firstname + lastname + email);
+            
+        $.ajax({
+        method: "POST",
+        url: "classes/User.php",
+        data: {firstname: firstname, lastname: lastname, email: email}
+        }).done(function(response){
+            //alert("DONE");
+        });
+        }
+    });
 });
