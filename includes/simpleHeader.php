4c21aea2bef5068bfaf21b0130ab0a346497719f
Tom Dirix <tomdirix@Tom-Dirix.local>
2017-05-03 14:58:14 +0200

    Tag toegevoegd

--- includes/simpleHeader.php
+++ includes/simpleHeader.php
@@ -1,4 +1,4 @@
 <header>
-	<img id="pijl" src="images/ic_terugpijl.svg" alt="vorige">
+	<a href="index.php"><img id="pijl" src="images/ic_terugpijl.svg" alt="vorige"></a>
 	<h1><?php echo SCHERM; ?></h1>
 </header>
