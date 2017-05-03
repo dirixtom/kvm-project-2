b84cabe34e41ba6929af28a6aecd0fc5ea40691f
Roel Van Rossen <roelifant@gmail.com>
2017-05-03 11:06:49 +0200

    opkuisen van code

--- ajax/ajaxProfile.php
+++ ajax/ajaxProfile.php
@@ -23,22 +23,7 @@
             }
 
             if(!empty($_POST['email'])){
-                /*$conn = Db::getInstance();
-                $checkemail = $conn->prepare("SELECT * FROM `users` WHERE (email =:email) and (username != :username)");
-                $checkemail->bindValue(":email", $_POST['email']);
-                $checkemail->bindValue(":username", $_SESSION['user']);
-                $checkemail->execute();
-                $found_email = $checkemail->fetch(PDO::FETCH_ASSOC);
-                $rows = count($found_email);
-                if (($found_email == true)) {
-                    throw new Exception("Deze email staat al geregistreerd op een ander account.");
-                }
-                if (strpos($_POST['email'], "@") && strpos($_POST['email'], ".")){*/
-                    $user->Email = $_POST['email'];
-                /*} else {
-                    throw new Exception("Dit is geen geldig email adres.");
-                }*/
-
+                $user->Email = $_POST['email'];
             } else {
                 $user->Email = $_SESSION['email'];
             }
@@ -46,29 +31,6 @@
             if(empty($_FILES['image'])){
                 $user->Image = $_SESSION['image'];
             }
-
-            /*if (!empty($_FILES['image']['name'])) {
-                $bestandsnaam = strtolower($_FILES['image']['name']);
-
-                if (strpos($bestandsnaam, ".png")) {
-                    move_uploaded_file($_FILES["image"]["tmp_name"],
-                    "uploads/profileImages/" . $_SESSION['userid'] . ".png");
-                    $user->Image = $_SESSION['userid'] . ".png";
-                } elseif (strpos($bestandsnaam, ".jpg")) {
-                    move_uploaded_file($_FILES["image"]["tmp_name"],
-                    "uploads/profileImages/" . $_SESSION['userid'] . ".jpg");
-                    $user->Image = $_SESSION['userid'] . ".jpg";
-                } elseif (strpos($bestandsnaam, ".gif")) {
-                    move_uploaded_file($_FILES["image"]["tmp_name"],
-                    "uploads/profileImages/" . $_SESSION['userid'] . ".gif");
-                    $user->Image = $_SESSION['userid'] . ".gif";
-                } else {
-                    throw new exception("De foto moet een jpg, png of gif zijn!");
-                }
-            } else {
-                $user->Image = $_SESSION['image'];
-            }*/
-
 
             if($user->updateProfile()){
                 $feedback = [
