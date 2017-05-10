957810f08c40fe092632989d7b6fc933433489c0
Tom Dirix <dirixtom@users.noreply.github.com>
2017-05-10 10:42:49 +0200

    Link fixes

--- includes/simpleHeader.php
+++ includes/simpleHeader.php
@@ -1,4 +1,4 @@
 <header>
-	<a href="index.php"><img id="pijl" src="images/ic_terugpijl.svg" alt="vorige"></a>
+	<a href="index.php"><img id="pijl" src="../images/ic_terugpijl.svg" alt="vorige"></a>
 	<h1><?php echo SCHERM; ?></h1>
 </header>
