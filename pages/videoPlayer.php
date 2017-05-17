0e26bde2279ec785a8b8a3e4451b4de4ba84e2e4
Roel Van Rossen <roelifant@gmail.com>
2017-05-17 10:57:35 +0200

    kleine correctie in record.php

--- pages/videoPlayer.php
+++ pages/videoPlayer.php
@@ -43,22 +43,9 @@
     <div id="videoscreen">
        <video id="video" width="320" height="480" autoplay></video>
    </div>
-   <button id="record"> neem op </button>
+   <input type="file" accept="image/*;capture=camera">
+   <button id="pause"> || </button>
     <p id="data"></p>
-   <br>
-   <div class="modal" id="upload-modal">
-        <form action="" method="post">
-                   <p>scheid de tags met een ; en een spatie</p>
-                    <input type="text" name="tags" id="tags" placeholder="voeg tags toe">
-                    <input type="hidden" name="cancel" id="cancel" value="true">
-                    <br>
-                    <button type="submit" id="cancel">video annuleren</button>
-                    <br>
-                    <button type="submit" id="upload">Ok</button>
-                </form>
-    </div>
    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
-    <script src="../js/record.js"></script>
-    <script src="../js/upload.js"></script>
 </body>
 </html>
