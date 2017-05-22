67c2a49c9116ab38005823e34e14c8d26c83cafb
Roel Van Rossen <roelifant@gmail.com>
2017-05-22 19:00:10 +0200

    email input in login.php

--- pages/login.php
+++ pages/login.php
@@ -25,6 +25,11 @@
 <head>
     <meta charset="UTF-8">
     <title><?php echo SCHERM; ?></title>
+    <style>
+        #wachtwoord_modal{
+            display: none;
+        }
+    </style>
 </head>
 <body>
     <a href="#"> Login </a>
@@ -39,6 +44,17 @@
     <h3> of </h3>
     <button>Log in met Facebook </button>
     <br>
-    <a href="#"> wachtwoord vergeten</a>
+    <a href="#" id="wachtwoord"> wachtwoord vergeten</a>
+    <div class="modal" id="wachtwoord_modal">
+       <h2> Wachtwoord vergeten </h2>
+       <p>Vul het e-mail adres in waarmee je je geregistreerd hebt.</p>
+        <form action="" method="post">
+            <input type="text" name="email" id="email" placeholder="E-mail">
+            <button id="cancel_wachtwoord"> annuleer </button>
+            <button type="submit"> ok </button>
+        </form>
+    </div>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
+    <script src="../js/login.js"></script>
 </body>
 </html>
