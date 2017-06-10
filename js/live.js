d113c1cd43c1be44bbdd0f9d9e9600a5480ccc85
Roel Van Rossen <roelifant@gmail.com>
2017-06-10 17:40:50 +0200

    live_alt triggeren met toetsenbord

--- js/live.js
+++ js/live.js
@@ -2,5 +2,11 @@ $(document).ready(function(){
     var channel = "PerzianX7"; // pas dit aan om een andere stream te tonen
     
     $("#twitch").html("<iframe src='http://player.twitch.tv/?channel="+channel+"&muted=true' height='360' width='570' frameborder='0' scrolling='no' allowfullscreen='false'> </iframe>");
+    
+    $(document).bind('keyup', function(e){
+    if(e.which==65) {
+      window.location = "live_alt.php";
+    }
+    });
 
 });
